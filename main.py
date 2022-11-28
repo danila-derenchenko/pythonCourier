6de958a7522ecadce03aee26564d6e445c3fd500
danila-derenchenko <79960110+danila-derenchenko@users.noreply.github.com>
2022-11-28 22:43:50 +0400

    curier basic

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
 import math
-
-orders = [[4, 5], [6, 9]]
+import random
 
 class Curier:
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
-    def __init__(self, x, y, name):
+    def __init__(self, x, y, name, id):
         self.coordinate = Point(x, y)
         self.name = name
-        self.coordinate.searchOrder()
+        self.id = id
+    def restartSearch(self):
+        self.searchOrders()
+    def searchOrders(self):
+        if len(orders) > 0:
+            minDist = 100000000000000000000000000000000
+            minInd = 0
+            curierCoordinate = self.coordinate.getCoordinate()
+            for i in orders:
+                if math.dist((i[0], i[1]), (curierCoordinate[0], curierCoordinate[1])) < minDist:
+                    minDist = math.dist((i[0], i[1]), (curierCoordinate[0], curierCoordinate[1]))
+                    minInd = orders.index(i)
+            order = orders[minInd]
+            orders.remove(order)
+            print(orders)
+            self.coordinate.relocate(order[0], order[1])
+            self.coordinate.relocate(0, 0)
+            print("Заказ доставлен")
+            self.restartSearch()
+        else:
+            print("Заказов больше нет")
 class Point:
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    def searchOrder(self):
-        min = 100000
-        ind = 100000
-        for i in range(len(orders)):
-            if math.dist((self.x, self.y), (i[0], i[1])) < min:
-                min = math.dist((self.x, self.y), (i[0], i[1]))
-                ind = i
-curier = Curier(4, 8, "Anton")
+    def relocate(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
+    def getCoordinate(self):
+        return [self.x, self.y]
+
+curiers = []
+orders = []
+
+for i in range(random.randint(4, 10)):
+    curiers.append(Curier(random.randint(0, 100), random.randint(0, 100), "Alexey", i + 1))
+
+for i in range(random.randint(4, 10)):
+    orders.append([random.randint(0, 100), random.randint(0, 100)])
+
+for i in curiers:
+    i.searchOrders()
