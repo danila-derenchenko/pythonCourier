afd35ce7730628121f54609a06eda0a5bcc215fa
danila-derenchenko <79960110+danila-derenchenko@users.noreply.github.com>
2022-11-30 01:27:17 +0400

    dorabot

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import random
 class Curier:
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
-    def __init__(self, x, y, name, id):
+    def __init__(self, x, y, name, id, speed):
         self.coordinate = Point(x, y)
         self.name = name
         self.id = id
+        self.speed = speed
+        self.history = History()
     def restartSearch(self):
         self.searchOrders()
     def searchOrders(self):
@@ -21,13 +23,15 @@ class Curier:
                     minInd = orders.index(i)
             order = orders[minInd]
             orders.remove(order)
-            print(orders)
+            timer = math.ceil((math.dist((curierCoordinate[0], curierCoordinate[1]), (order[0], order[1])) + math.dist((order[0], order[1]), (0, 0))) / self.speed)
+            self.history.addOrder(math.ceil(math.dist((curierCoordinate[0], curierCoordinate[1]), (order[0], order[1])) / self.speed), {"x": curierCoordinate[0], "y": curierCoordinate[1]}, {"x": order[0], "y": order[1]}, "Доставка")
             self.coordinate.relocate(order[0], order[1])
+            self.history.addOrder(math.ceil(math.dist((order[0], order[1]), (0, 0)) / self.speed), {"x": order[0], "y": order[1]}, {"x": 0, "y": 0}, "Доставка")
             self.coordinate.relocate(0, 0)
-            print("Заказ доставлен")
-            self.restartSearch()
+            print("Заказ доставлен за время " + str(timer) + " " + str(self.id))
         else:
-            print("Заказов больше нет")
+            print("Заказов больше нет. Курьер " + str(self.id))
+            self.history.getHistory()
 class Point:
     def __new__(cls, *args, **kwargs):
         return super().__new__(cls)
@@ -40,14 +44,29 @@ class Point:
     def getCoordinate(self):
         return [self.x, self.y]
 
+class History:
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+    def __init__(self):
+        self.history = []
+    def addOrder(self, timer, fromStart, toFinish, orders):
+        self.history.append({
+            "time": timer,
+            "from": fromStart,
+            "to": toFinish,
+            "orders": orders
+        })
+    def getHistory(self):
+        print(self.history)
 curiers = []
 orders = []
 
-for i in range(random.randint(4, 10)):
-    curiers.append(Curier(random.randint(0, 100), random.randint(0, 100), "Alexey", i + 1))
+for i in range(random.randint(5, 10)):
+    curiers.append(Curier(random.randint(0, 100), random.randint(0, 100), "Alexey", i + 1, random.randint(4, 8)))
 
-for i in range(random.randint(4, 10)):
+for i in range(10):
     orders.append([random.randint(0, 100), random.randint(0, 100)])
 
-for i in curiers:
-    i.searchOrders()
+while len(orders) > 0:
+    for i in curiers:
+        i.searchOrders()
